aa1c42df8f023f55b1d6ba8f1589ee07e8a2755d
awasevalentine <54159265+awasevalentine@users.noreply.github.com>
2022-04-29 21:16:15 +0100

    Enable cor for cross origin resource sharing

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+});
+
+app.enableCors({
+    allowedHeaders:"*",
+    origin: "*"
+});
   await app.listen(process.env.PORT || 3000, ()=> {
     console.log("Server Running on port 3000")
   });
