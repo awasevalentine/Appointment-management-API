bf0c2c96c6bea983e58a098de32f9bb8b2b5adf9
awasevalentine <54159265+awasevalentine@users.noreply.github.com>
2022-04-30 04:34:55 +0100

    added new herouk application

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port = process.env.PORT || 3000
-  await app.listen(port, "0.0.0.0", ()=> {
+  await app.listen(parseInt(process.env.PORT) || 3000, ()=> {
       console.log("Server Running on port 3000")
     });
   // await app.listen( process.env.PORT, ()=> {
