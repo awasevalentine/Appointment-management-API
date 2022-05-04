7094c8b49df535e04fe1589f47e4be4789e735d0
awasevalentine <54159265+awasevalentine@users.noreply.github.com>
2022-05-04 12:46:20 +0100

    Modified database connection param

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // const port = parseInt(process.env.PORT, 10) || 3000
-  const port = process.env.PORT;
+  const port = process.env.PORT || 5000;
   const host = '0.0.0.0'
-  await app.listen(port, host, ()=> {
-      console.log("Server Running on port 3000")
-    });
+  // await app.listen(port, host, ()=> {
+  //   console.log("Server Running on port 3000")
+  // });
+  await app.listen(port, ()=> {
+    console.log("Server Running on port 3000")
+  });
   // await app.listen( process.env.PORT, ()=> {
   //   console.log("Server Running on port", process.env.PORT || 8080)
   // });
