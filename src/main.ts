8e8ade632b108b6aed4697305ea736f3df94da2e
awasevalentine <54159265+awasevalentine@users.noreply.github.com>
2022-05-04 15:50:18 +0100

    Modified database connection param add some port

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // const port = parseInt(process.env.PORT, 10) || 3000
-  const port = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
   const host = process.env.HOST || '0.0.0.0'
-  await app.listen(port, host, ()=> {
-    console.log(`Server Running with  port ${port}`)
-  });
- 
+  // await app.listen(PORT, host, ()=> {
+  //   console.log(`Server Running with  port ${PORT}`)
+  // });
+  await app.listen(PORT);
+
   
 // await http.createServer.listen(port, host, ()=> {
 //     console.log("Server Running on port 3000")
